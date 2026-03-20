9fc1ac0196966719ea92185b9f33a600967636f5
Andrea <a.memba@alustudent.com>
2026-03-21 00:22:42 +0400

    created database

---
+++ database/database_setup.sql
@@ -0,0 +1,9 @@
+-- ============================================================
+-- MoMo SMS Data Processing System - Database Setup Script
+-- ============================================================
+
+CREATE DATABASE IF NOT EXISTS momo_sms_db
+  CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci;
+
+USE momo_sms_db;
